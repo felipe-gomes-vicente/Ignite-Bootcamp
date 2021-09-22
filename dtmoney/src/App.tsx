a3caab66ece0da9329f22b00805a1841808baae8
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-09-22 16:37:28 -0300

    Componente: Header

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,14 +1,11 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257E6;
-`
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <Title className="title">Hello World</Title>
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
