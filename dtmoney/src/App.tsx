3f0e3503b83924d62ae11d9a67007c14371529f5
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-09-22 15:02:13 -0300

    styled components

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257E6;
+`
+
+export function App() {
+  return (
+    <div className="App">
+      <Title className="title">Hello World</Title>
+    </div>
+  );
+}
