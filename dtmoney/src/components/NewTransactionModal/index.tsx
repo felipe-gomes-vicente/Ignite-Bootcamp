0464a6d6ba5c311703f0063d8c51bd542659780a
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-09-24 12:05:03 -0300

    Formatando valores

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { api } from "../../services/api";
 
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
@@ -21,8 +22,15 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({title, value, category, type})
-  }
+    const data = {
+      title, 
+      value, 
+      category, 
+      type
+    };
+
+    api.post('/transactions', data)
+  };
 
   return (
     <Modal
